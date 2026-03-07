26a46484383799c7869930e297f06ba108a23823
BlueMoonHunt <hkumar.bluemoonhunt@gmail.com>
2026-03-07 14:41:09 +0530

    .

--- build.bat
+++ build.bat
@@ -5,8 +5,9 @@ if "%WORKSPACE_DIR:~-1%"=="\" set "WORKSPACE_DIR=%WORKSPACE_DIR:~0,-1%"
 
 set "INCLUDES= -I %WORKSPACE_DIR%"
 set "OPTIONS= /std:c++20 -DUNICODE -D_UNICODE -Zi /GF /GR"
+set "BUILD_DIR=%WORKSPACE_DIR%\build"
 
-mkdir build
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
 pushd build
 
 cl %OPTIONS% %INCLUDES% %WORKSPACE_DIR%\main.cpp
