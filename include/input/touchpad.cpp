a75bdd348ad8269633ace754102212286f4a3472
AureNoctis <blockpanda95@gmail.com>
2026-06-26 16:11:34 +0530

    completed filling delta struct stage

--- include/input/touchpad.cpp
+++ include/input/touchpad.cpp
@@ -5,272 +5,234 @@
 #include "utils/usage.h"
 #include "math.h"
 #include <cstring>
+#include <Windows.h>
 
 
 [[maybe_unused]]
 void printTouchpadData(HWND window) {
-    Window_state*  w_state     = (Window_state*)GetWindowLongPtrW(window, GWLP_USERDATA);
-    TouchPad_state t_state     = w_state->t_state;
-    Finger*        finger_data = w_state->finger_data;
+	Window_state*  w_state	   = (Window_state*)GetWindowLongPtrW(window, GWLP_USERDATA);
+	TouchPad_state t_state	   = w_state->t_state;
+	Finger*		   finger_data = w_state->finger_data;
 
-    printf("---------------------------------------------------\n"
-           "contact count  :  %hhu \n"
-           "scan time      :  %hu x10^-4\n"
-           "button         :  %hhu \n",
-           t_state.contactCount, t_state.scanTime, t_state.touchPadButton);
+	printf("---------------------------------------------------\n"
+		   "contact count  :  %hhu \n"
+		   "scan time      :  %hu x10^-4\n"
+		   "button         :  %hhu \n",
+		   t_state.contactCount, t_state.scanTime, t_state.touchPadButton);
 
-    printf("Finger        Tip        Confidence        ID        X              Y\n");
+	printf("Finger        Tip        Confidence        ID        X              Y\n");
 
-    for (i32 i = 0; i < 5; i++) {
-        printf("F%-2i          %-3hu        %-10hu        %-3hhu       %-10u     %-10u\n", i + 1, finger_data[i].tip_switch, finger_data[i].confidence,
-               finger_data[i].id, finger_data[i].x, finger_data[i].y);
-    }
+	for (i32 i = 0; i < 5; i++) {
+		printf("F%-2i          %-3hu        %-10hu        %-3hhu       %-10u     %-10u\n", i + 1, finger_data[i].tip_switch, finger_data[i].confidence,
+			   finger_data[i].id, finger_data[i].x, finger_data[i].y);
+	}
 }
 
 void getFingerData(HWND window) {
-    Window_state*        w_state       = (Window_state*)GetWindowLongPtrW(window, GWLP_USERDATA);
-    PHIDP_PREPARSED_DATA preparsedData = w_state->input_report_info.ptrPreparsedData;
-    Finger*              finger_data   = w_state->finger_data;
-    TouchPad_state       t_state       = w_state->t_state;
+	Window_state*		 w_state	   = (Window_state*)GetWindowLongPtrW(window, GWLP_USERDATA);
+	PHIDP_PREPARSED_DATA preparsedData = w_state->input_report_info.ptrPreparsedData;
+	Finger*				 finger_data   = w_state->finger_data;
+	TouchPad_state		 t_state	   = w_state->t_state;
 
-    RAWINPUT* raw       = w_state->raw_input;
-    i8*       report    = (i8*)raw->data.hid.bRawData;
-    u32       reportLen = raw->data.hid.dwSizeHid;
+	RAWINPUT* raw		= w_state->raw_input;
+	i8*		  report	= (i8*)raw->data.hid.bRawData;
+	u32		  reportLen = raw->data.hid.dwSizeHid;
 
-    memset(finger_data, 0, sizeof(Finger) * 5);
-    memset(&t_state, 0, sizeof(TouchPad_state));
+	memset(finger_data, 0, sizeof(Finger) * 5);
+	memset(&t_state, 0, sizeof(TouchPad_state));
 
-    HIDP_DATA data[32];
-    u32       data_length = 32;
-    HidP_GetData(HidP_Input, data, (unsigned long*)&data_length, preparsedData, (char*)report, reportLen);
+	HIDP_DATA data[32];
+	u32		  data_length = 32;
+	HidP_GetData(HidP_Input, data, (unsigned long*)&data_length, preparsedData, (char*)report, reportLen);
 
+	for (u32 i = 0; i < data_length; i++) {
+		switch (data[i].DataIndex) {
+				// clang-format off
 
-    for (u32 i = 0; i < data_length; i++) {
-        switch (data[i].DataIndex) {
-            // touchpad
-            case 0: {
-                t_state.touchPadButton = data[i].On;
-            } break;
-            case 1: {
-                t_state.contactCount = data[i].RawValue;
-            } break;
-            case 2: {
-                t_state.scanTime = data[i].RawValue;
-            } break;
+			// touchpad
+			case 0: { t_state.touchPadButton = data[i].On;       } break;
+			case 1: { t_state.contactCount   = data[i].RawValue; } break;
+			case 2: { t_state.scanTime       = data[i].RawValue; } break;
 
+			// {F1 F2 ...} are relative to touchpad:
+			// index0
+			case 3:  { finger_data[F1].tip_switch = data[i].On;       } break;
+			case 4:  { finger_data[F1].confidence = data[i].On;       } break;
+			case 5:  { finger_data[F1].id         = data[i].RawValue; } break;
+			case 6:  { finger_data[F1].x          = data[i].RawValue; } break;
+			case 7:  { finger_data[F1].y          = data[i].RawValue; } break;
 
-            // {F1 F2 ...} are realtive to touchpad:
-            // index0
-            case 3: {
-                finger_data[F1].tip_switch = data[i].On;
-            } break;
-            case 4: {
-                finger_data[F1].confidence = data[i].On;
-            } break;
-            case 5: {
-                finger_data[F1].id = data[i].RawValue;
-            } break;
-            case 6: {
-                finger_data[F1].x = data[i].RawValue;
-            } break;
-            case 7: {
-                finger_data[F1].y = data[i].RawValue;
-            } break;
+			// index1
+			case 8:  { finger_data[F2].tip_switch = data[i].On;       } break;
+			case 9:  { finger_data[F2].confidence = data[i].On;       } break;
+			case 10: { finger_data[F2].id         = data[i].RawValue; } break;
+			case 11: { finger_data[F2].x          = data[i].RawValue; } break;
+			case 12: { finger_data[F2].y          = data[i].RawValue; } break;
 
-            // index1
-            case 8: {
-                finger_data[F2].tip_switch = data[i].On;
-            } break;
-            case 9: {
-                finger_data[F2].confidence = data[i].On;
-            } break;
-            case 10: {
-                finger_data[F2].id = data[i].RawValue;
-            } break;
-            case 11: {
-                finger_data[F2].x = data[i].RawValue;
-            } break;
-            case 12: {
-                finger_data[F2].y = data[i].RawValue;
-            } break;
+			// index2
+			case 13: { finger_data[F3].tip_switch = data[i].On;       } break;
+			case 14: { finger_data[F3].confidence = data[i].On;       } break;
+			case 15: { finger_data[F3].id         = data[i].RawValue; } break;
+			case 16: { finger_data[F3].x          = data[i].RawValue; } break;
+			case 17: { finger_data[F3].y          = data[i].RawValue; } break;
 
-            // index2
-            case 13: {
-                finger_data[F3].tip_switch = data[i].On;
-            } break;
-            case 14: {
-                finger_data[F3].confidence = data[i].On;
-            } break;
-            case 15: {
-                finger_data[F3].id = data[i].RawValue;
-            } break;
-            case 16: {
-                finger_data[F3].x = data[i].RawValue;
-            } break;
-            case 17: {
-                finger_data[F3].y = data[i].RawValue;
-            } break;
+			// index3
+			case 18: { finger_data[F4].tip_switch = data[i].On;       } break;
+			case 19: { finger_data[F4].confidence = data[i].On;       } break;
+			case 20: { finger_data[F4].id         = data[i].RawValue; } break;
+			case 21: { finger_data[F4].x          = data[i].RawValue; } break;
+			case 22: { finger_data[F4].y          = data[i].RawValue; } break;
 
-            // index3
-            case 18: {
-                finger_data[F4].tip_switch = data[i].On;
-            } break;
-            case 19: {
-                finger_data[F4].confidence = data[i].On;
-            } break;
-            case 20: {
-                finger_data[F4].id = data[i].RawValue;
-            } break;
-            case 21: {
-                finger_data[F4].x = data[i].RawValue;
-            } break;
-            case 22: {
-                finger_data[F4].y = data[i].RawValue;
-            } break;
+			// index4
+			case 23: { finger_data[F5].tip_switch = data[i].On;       } break;
+			case 24: { finger_data[F5].confidence = data[i].On;       } break;
+			case 25: { finger_data[F5].id         = data[i].RawValue; } break;
+			case 26: { finger_data[F5].x          = data[i].RawValue; } break;
+			case 27: { finger_data[F5].y          = data[i].RawValue; } break;
 
-            // index4
-            case 23: {
-                finger_data[F5].tip_switch = data[i].On;
-            } break;
-            case 24: {
-                finger_data[F5].confidence = data[i].On;
-            } break;
-            case 25: {
-                finger_data[F5].id = data[i].RawValue;
-            } break;
-            case 26: {
-                finger_data[F5].x = data[i].RawValue;
-            } break;
-            case 27: {
-                finger_data[F5].y = data[i].RawValue;
-            } break;
+			default: { fprintf(stderr, "unknown data index: %d found while filling data\n", data[i].DataIndex); } break;
 
-            default: {
-                fprintf(stderr, "unknown data index: %d found while filling data\n", data[i].DataIndex);
-            } break;
-        }
-    }
+				// clang-format on
+		}
+	}
 
-    w_state->gesture_end = !(
-        bool)((finger_data[0].tip_switch | finger_data[1].tip_switch | finger_data[2].tip_switch | finger_data[3].tip_switch | finger_data[4].tip_switch));
-    w_state->gesture_start = !w_state->gesture_end;
+	w_state->gesture_end = !(
+		bool)((finger_data[0].tip_switch | finger_data[1].tip_switch | finger_data[2].tip_switch | finger_data[3].tip_switch | finger_data[4].tip_switch));
+	w_state->gesture_start = !w_state->gesture_end;
 }
 
 
-int fillDeltaStruct(Finger* ga_pf_start_data, Finger* ga_pf_end_data, u16* ga_pf_start_time, u16 ga_end_time, FingerDeltaData* ga_pf_delta_data) {
+u8 fillDeltaStruct(Finger* ga_pf_start_data, Finger* ga_pf_end_data, u16* ga_pf_start_time, u16 ga_end_time, FingerDeltaData* ga_pf_delta_data) {
 #define Short_max 65536
-    for (int i = 0; i < 5; i++) {
-        ga_pf_delta_data[i].xi                = ga_pf_start_data[i].x;
-        ga_pf_delta_data[i].yi                = ga_pf_start_data[i].y;
-        ga_pf_delta_data[i].xf                = ga_pf_end_data[i].x;
-        ga_pf_delta_data[i].yf                = ga_pf_end_data[i].y;
-        ga_pf_delta_data[i].xd                = ga_pf_delta_data[i].xf - ga_pf_delta_data[i].xi;
-        ga_pf_delta_data[i].yd                = ga_pf_delta_data[i].yf - ga_pf_delta_data[i].yi;
-        ga_pf_delta_data[i].confidence        = ga_pf_start_data[i].confidence;
-        ga_pf_delta_data[i].contact_state     = (Contact_state)ga_pf_end_data[i].tip_switch;
-        ga_pf_delta_data[i].startTime         = ga_pf_start_time[i];
-        ga_pf_delta_data[i].deltaTime         = (Short_max + ga_end_time - ga_pf_start_time[i]) % Short_max;
-        ga_pf_delta_data[i].distance_traveled = (u32)hypot(ga_pf_delta_data[i].xd, ga_pf_delta_data[i].yd);
-    }
+	u8 finger_state = 0;
+	for (int i = 0; i < 5; i++) {
+		if (ga_pf_start_data[i].confidence == 1) {
+			ga_pf_delta_data[i].xi				  = ga_pf_start_data[i].x;
+			ga_pf_delta_data[i].yi				  = ga_pf_start_data[i].y;
+			ga_pf_delta_data[i].xf				  = ga_pf_end_data[i].x;
+			ga_pf_delta_data[i].yf				  = ga_pf_end_data[i].y;
+			ga_pf_delta_data[i].xd				  = ga_pf_delta_data[i].xf - ga_pf_delta_data[i].xi;
+			ga_pf_delta_data[i].yd				  = ga_pf_delta_data[i].yf - ga_pf_delta_data[i].yi;
+			ga_pf_delta_data[i].confidence		  = ga_pf_start_data[i].confidence;
+			ga_pf_delta_data[i].contact_state	  = (Contact_state)ga_pf_end_data[i].tip_switch;
+			ga_pf_delta_data[i].startTime		  = ga_pf_start_time[i];
+			ga_pf_delta_data[i].deltaTime		  = (Short_max + ga_end_time - ga_pf_start_time[i]) % Short_max;
+			ga_pf_delta_data[i].distance_traveled = (u32)hypot(ga_pf_delta_data[i].xd, ga_pf_delta_data[i].yd);
+
+			if (ga_pf_delta_data[i].contact_state == DOWN)
+				SET_BIT(&finger_state, i);
+		}
+	}
+	return finger_state;
 #undef Short_max
 }
 
 void getFingerDeltaData(HWND window) {
-    Window_state* w_state = (Window_state*)GetWindowLongPtrW(window, GWLP_USERDATA);
+	Window_state* w_state = (Window_state*)GetWindowLongPtrW(window, GWLP_USERDATA);
 
-    /*
-     naming convention:
-     g  -> gesture
-     a  -> atom
-     pf -> per finger
+	/*
+	 naming convention:
+	 g  -> gesture
+	 a  -> atom
+	 pf -> per finger
 
-     geture = collection of ga(s)
-     */
+	 geture = collection of ga(s)
+	 */
 
-    static u16 g_start_time = 0;
+	//	static u16 g_start_time = 0;
 
-    static Finger ga_pf_start_data[5];
-    static u16    ga_pf_start_time[5];
-    static bool   got_ga_pf_start_data[5]; // for both time and data
-    static u8     ga_prev_contact_count    = 0;
-    static u8     ga_current_contact_count = 0;
+	static Finger ga_pf_start_data[5]	   = {};
+	static u16	  ga_pf_start_time[5]	   = {};
+	static u8	  got_ga_pf_start_data[5]  = {}; // for both time and data
+	static u8	  ga_prev_contact_count	   = 0;
+	static u8	  ga_current_contact_count = 0;
 
-    getFingerData(window);
+	getFingerData(window);
 
-    Finger*        finger_data = w_state->finger_data;
-    TouchPad_state t_state     = w_state->t_state;
-    ga_current_contact_count   = t_state.contactCount;
-
-
-    if (ga_current_contact_count < ga_prev_contact_count || w_state->gesture_end) {
-        // flush data
-        fillDeltaStruct(ga_pf_start_data, finger_data, ga_pf_start_time, t_state.scanTime, w_state->finger_delta);
-        PostMessageW(window, GRE_GA_DELTA_READY, ga_prev_contact_count, 0);
-
-        if (w_state->gesture_end)
-            PostMessageW(window, GRE_GESTURE_END, g_start_time, 0);
-    }
+	Finger*		   finger_data = w_state->finger_data;
+	TouchPad_state t_state	   = w_state->t_state;
+	ga_current_contact_count   = t_state.contactCount;
 
 
-    // if(w_state->gesture_start == true && gesture_start_time == 0 )
-    for (int i = 0; i < 5; i++) {
-        if (finger_data[i].confidence != 0) {
-            if (got_ga_pf_start_data[finger_data[i].id] == false) {
-                got_ga_pf_start_data[finger_data[i].id] = true;
-                memcpy(ga_pf_start_data + i, finger_data + i, sizeof(Finger));
-                ga_pf_start_time[i] = t_state.scanTime;
-            }
-            continue;
-        }
-        break;
-    }
+	if (ga_current_contact_count < ga_prev_contact_count || w_state->gesture_end) {
+		// flush data
+		u8 finger_state = fillDeltaStruct(ga_pf_start_data, finger_data, ga_pf_start_time, t_state.scanTime, w_state->finger_delta);
+		for (int i = 0; i < 5; i++) {
+			if ((got_ga_pf_start_data[i] &= ((finger_state >> i) & 1)) == 0) {
+				// to remove ghost finger:
+				// 3 finger --> 2 finger --> 1 finger
+				// the data of 1st removed finger is still there(confidence = 1), so it will again be filled in delta struct:
+				memset(ga_pf_start_data + i, 0, sizeof(Finger));
+			}
+		}
+
+		PostMessageW(window, GRE_GA_DELTA_READY, ga_prev_contact_count, 0);
+
+		if (w_state->gesture_end)
+			PostMessageW(window, GRE_GESTURE_END, 0, 0);
+	}
 
 
-    ga_prev_contact_count = ga_current_contact_count;
-    //     if(t_state.contactCount > *get_maxContactCount(holder)){
-    //         *get_maxContactCount(holder) = t_state.contactCount;
-    //     }
-    //
-    //     for(int i = 0; i < 5; i++){
-    //         if(finger_data[i].confidence != 0){
-    //             if(got_start_time[finger_data[i].id] != true){
-    //                 gesture_start_time[finger_data[i].id] = t_state.scanTime;
-    //                 got_start_time[finger_data[i].id] = true;
-    //             }
-    //             continue;
-    //         }
-    //         break;
-    //     }
-    //
-    //
-    //     if(gesture_start_counter == 2){
-    //         memcpy(gesture_start_data, finger_data, 5 * sizeof(Finger));
-    //         return;
-    //     }
-    //     if(gesture_end){
-    //         for(int i = 0; i < 5; i++){
-    //             holder[i].xi = gesture_start_data[i].x;
-    //             holder[i].yi = gesture_start_data[i].y;
-    //             holder[i].xf = finger_data[i].x;
-    //             holder[i].yf = finger_data[i].y;
-    //             holder[i].xd = holder[i].xf - holder[i].xi;
-    //             holder[i].yd = holder[i].yf - holder[i].yi;
-    //
-    //             holder[i].confidence = gesture_start_data[i].confidence;
-    //             holder[i].tip_switch = gesture_start_data[i].tip_switch;
-    //             holder[i].startTime = gesture_start_time[i];
-    //
-    // #define Short_max 65536
-    //             holder[i].deltaTime = (Short_max + t_state.scanTime - gesture_start_time[i]) % Short_max;
-    // #undef Short_max
-    //
-    //             holder[i].distance_traveled = (u32)hypot(holder[i].xd, holder[i].yd);
-    //         }
-    //         memset(gesture_start_data, 0, sizeof(gesture_start_data));
-    //         memset(gesture_start_time, 0, sizeof(gesture_start_time));
-    //         memset(got_start_time, 0, sizeof(got_start_time));
-    //     }
-    //
+	// if(w_state->gesture_start == true && gesture_start_time == 0 )
+	for (int i = 0; i < 5; i++) {
+		if (finger_data[i].confidence != 0) {
+			int finger_ID = finger_data[i].id;
+			if (got_ga_pf_start_data[finger_ID] == 0) {
+				got_ga_pf_start_data[finger_ID] = 1;
+				memcpy(ga_pf_start_data + finger_ID, finger_data + i, sizeof(Finger));
+				ga_pf_start_time[finger_ID] = t_state.scanTime;
+			}
+			continue;
+		}
+		break;
+	}
+
+
+	ga_prev_contact_count = ga_current_contact_count;
+	//     if(t_state.contactCount > *get_maxContactCount(holder)){
+	//         *get_maxContactCount(holder) = t_state.contactCount;
+	//     }
+	//
+	//     for(int i = 0; i < 5; i++){
+	//         if(finger_data[i].confidence != 0){
+	//             if(got_start_time[finger_data[i].id] != true){
+	//                 gesture_start_time[finger_data[i].id] = t_state.scanTime;
+	//                 got_start_time[finger_data[i].id] = true;
+	//             }
+	//             continue;
+	//         }
+	//         break;
+	//     }
+	//
+	//
+	//     if(gesture_start_counter == 2){
+	//         memcpy(gesture_start_data, finger_data, 5 * sizeof(Finger));
+	//         return;
+	//     }
+	//     if(gesture_end){
+	//         for(int i = 0; i < 5; i++){
+	//             holder[i].xi = gesture_start_data[i].x;
+	//             holder[i].yi = gesture_start_data[i].y;
+	//             holder[i].xf = finger_data[i].x;
+	//             holder[i].yf = finger_data[i].y;
+	//             holder[i].xd = holder[i].xf - holder[i].xi;
+	//             holder[i].yd = holder[i].yf - holder[i].yi;
+	//
+	//             holder[i].confidence = gesture_start_data[i].confidence;
+	//             holder[i].tip_switch = gesture_start_data[i].tip_switch;
+	//             holder[i].startTime = gesture_start_time[i];
+	//
+	// #define Short_max 65536
+	//             holder[i].deltaTime = (Short_max + t_state.scanTime - gesture_start_time[i]) % Short_max;
+	// #undef Short_max
+	//
+	//             holder[i].distance_traveled = (u32)hypot(holder[i].xd, holder[i].yd);
+	//         }
+	//         memset(gesture_start_data, 0, sizeof(gesture_start_data));
+	//         memset(gesture_start_time, 0, sizeof(gesture_start_time));
+	//         memset(got_start_time, 0, sizeof(got_start_time));
+	//     }
+	//
 }
 
 #endif
--- include/utils/declaration.h
+++ include/utils/declaration.h
@@ -24,10 +24,12 @@ typedef long NTSTATUS;
 #define GRE_GESTURE_END (WM_APP + 2)
 #define COMBINE_64(low, high) ((((u64)(high)) << 32) | ((u32)(low)))
 #define SPLIT_64(group, low_ptr, high_ptr)                                                                                                                \
-    do {                                                                                                                                                  \
-        *(high_ptr) = (u32)((group) >> 32);                                                                                                               \
-        *(low_ptr)  = (u32)((group) & 0xFFFFFFFFULL);                                                                                                     \
-    } while (0)
+	do {                                                                                                                                                  \
+		*(high_ptr) = (u32)((group) >> 32);                                                                                                               \
+		*(low_ptr)	= (u32)((group) & 0xFFFFFFFFULL);                                                                                                     \
+	} while (0)
+
+#define SET_BIT(host_ptr, bit) (*(host_ptr) |= (1ULL << bit))
 
 #define delta_t 70 // manually got this data for touchpad :) -> 70 us
 #define frequency 143.85
@@ -55,11 +57,11 @@ typedef long NTSTATUS;
 
 // ===================  typedefs  ==================
 
-typedef int8_t   i8;
-typedef int16_t  i16;
-typedef int32_t  i32;
-typedef int64_t  i64;
-typedef uint8_t  u8;
+typedef int8_t	 i8;
+typedef int16_t	 i16;
+typedef int32_t	 i32;
+typedef int64_t	 i64;
+typedef uint8_t	 u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
@@ -70,91 +72,91 @@ typedef double f64;
 
 // ===================  enum  ===================
 enum Finger_index : u8 {
-    F1 = 0,
-    F2,
-    F3,
-    F4,
-    F5,
-    TOTAL_FINGERS
+	F1 = 0,
+	F2,
+	F3,
+	F4,
+	F5,
+	TOTAL_FINGERS
 };
 
 enum Gesture_type : u8 {
-    IDLE = 0,
-    SWIPE,
-    MOVE,
-    TAP,
-    HOLD,
-    LONG_HOLD, // TODO: add the threshold values
-    UNCLEAR
+	IDLE = 0,
+	SWIPE,
+	MOVE,
+	TAP,
+	HOLD,
+	LONG_HOLD, // TODO: add the threshold values
+	UNCLEAR
 };
 
 enum Contact_state : u8 {
-    UP = 0,
-    DOWN
+	UP = 0,
+	DOWN
 };
 // ===================  structs  ==================
 struct InputReportInfo {
-    PHIDP_PREPARSED_DATA       ptrPreparsedData;
-    HIDP_CAPS*                 pCaps;
-    HIDP_VALUE_CAPS*           pValCaps;
-    HIDP_BUTTON_CAPS*          pButtonCaps;
-    HIDP_LINK_COLLECTION_NODE* pLinkCollection;
+	PHIDP_PREPARSED_DATA	   ptrPreparsedData;
+	HIDP_CAPS*				   pCaps;
+	HIDP_VALUE_CAPS*		   pValCaps;
+	HIDP_BUTTON_CAPS*		   pButtonCaps;
+	HIDP_LINK_COLLECTION_NODE* pLinkCollection;
 
-    HANDLE deviceHandle;
+	HANDLE deviceHandle;
 };
 
 struct offscreen_buffer {
-    BITMAPINFO info;   // This is you telling Windows the "Rules" of your DIB
-    void*      memory; // A raw pointer for you to touch the pixels directly
-    int        width;
-    int        height;
-    int        bytesPerPixel = 4;
-    int        pitch; // gap between tow rows
+	BITMAPINFO info;   // This is you telling Windows the "Rules" of your DIB
+	void*	   memory; // A raw pointer for you to touch the pixels directly
+	int		   width;
+	int		   height;
+	int		   bytesPerPixel = 4;
+	int		   pitch; // gap between tow rows
 };
 
 struct window_dimension {
-    int width;
-    int height;
+	int width;
+	int height;
 };
 
 struct Finger {
-    u32 x;
-    u32 y;
-    u8  id;
-    u8  tip_switch;
-    u8  confidence;
+	u32 x;
+	u32 y;
+	u8	id;
+	u8	tip_switch;
+	u8	confidence;
 };
 struct TouchPad_state {
-    u16 scanTime;
-    u8  contactCount;
-    u8  touchPadButton;
+	u16 scanTime;
+	u8	contactCount;
+	u8	touchPadButton;
 };
 
 struct FingerDeltaData {
-    u16 startTime; // x10^-4 sec
-    u16 deltaTime;
+	u16 startTime; // x10^-4 sec
+	u16 deltaTime;
 
-    u32 xi, yi;
-    u32 xf, yf;
-    u32 xd, yd;
-    u32 distance_traveled;
+	u32 xi, yi;
+	u32 xf, yf;
+	u32 xd, yd;
+	u32 distance_traveled;
 
-    u8            confidence;
-    Contact_state contact_state;
+	u8			  confidence;
+	Contact_state contact_state;
 };
 
 // TODO : order this according to size and alignment
 struct Window_state {
-    offscreen_buffer back_buffer;
-    InputReportInfo  input_report_info;
-    RAWINPUT*        raw_input;
-    Finger           finger_data[5];
-    FingerDeltaData  finger_delta[5];
-    TouchPad_state   t_state;
-    bool             gesture_start;
-    bool             gesture_end;
-    bool             running;
-    int              gesture_start_counter;
+	offscreen_buffer back_buffer;
+	InputReportInfo	 input_report_info;
+	RAWINPUT*		 raw_input;
+	Finger			 finger_data[5];
+	FingerDeltaData	 finger_delta[5];
+	TouchPad_state	 t_state;
+	bool			 gesture_start;
+	bool			 gesture_end;
+	bool			 running;
+	int				 gesture_start_counter;
 };
 
 
@@ -167,25 +169,25 @@ void getUsageValue_status(NTSTATUS status);
 // --- window specific ---
 LRESULT CALLBACK mainWindowCallback(HWND window, UINT message, WPARAM wParam, LPARAM lParam);
 window_dimension getWindowDimensions(HWND window);
-void             renderWeirdGradiant(offscreen_buffer* buffer, int blueOffset, int greenOffset);
-void             resizeDIBSection(offscreen_buffer* buffer, int width, int height);
-void             updateWindow(HDC deviceContext, int width, int height, offscreen_buffer* buffer);
-void*            _init_window_state(HWND window, Window_state w_state);
+void			 renderWeirdGradiant(offscreen_buffer* buffer, int blueOffset, int greenOffset);
+void			 resizeDIBSection(offscreen_buffer* buffer, int width, int height);
+void			 updateWindow(HDC deviceContext, int width, int height, offscreen_buffer* buffer);
+void*			 _init_window_state(HWND window, Window_state w_state);
 #define init_window_state(window, ...) _init_window_state(window, (Window_state){__VA_ARGS__})
 
 // --- hid_data extraction and consumption ---
 void getInputReportInfo(InputReportInfo* info);
-int  getRawData(HWND window, LPARAM lParam);
+int	 getRawData(HWND window, LPARAM lParam);
 void getFingerData(HWND window);
 
 // --- export hid_data/hid_info ---
-void              printTouchpadData(HWND window);
+void			  printTouchpadData(HWND window);
 force_Inline void getTouchPadInfoFile(InputReportInfo* info);
 
 // --- holder ---
 FingerDeltaData* create_holder(u32 data_size, u32 data_count);
-u32*             get_maxContactCount(FingerDeltaData* holder);
-void             free_holder(FingerDeltaData* holder);
+u32*			 get_maxContactCount(FingerDeltaData* holder);
+void			 free_holder(FingerDeltaData* holder);
 
 
 #endif
